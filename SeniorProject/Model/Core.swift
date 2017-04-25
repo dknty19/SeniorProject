a45131fbb6d808c22b7d36b34512718e38597b22
Vinh NGUYEN <vinhvernh.nguyen@tilden-newmacmini.enclave.com>
2017-04-25 10:50:41 +0700

    add-to-cart

---
+++ SeniorProject/Model/Core.swift
@@ -0,0 +1,13 @@
+//
+//  Core.swift
+//  SeniorProject
+//
+//  Created by Vinh (Vern) H. NGUYEN on 4/20/17.
+//  Copyright © 2017 Vinh (Vern) H. NGUYEN. All rights reserved.
+//
+
+import Foundation
+
+
+var superCart = [Cart]()
+var tableNumber = "1"
